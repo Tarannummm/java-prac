fd32e2d47510c74c34370b20fa9dd00c01a8597b
Tarannummm <tarannumelina@gmail.com>
2025-09-04 23:30:19 +0600

    exceptionHandling

---
+++ exceptionhandling.java
@@ -0,0 +1,76 @@
+// 1: Exception
+public class Main
+{
+	public static void main(String[] args) {
+
+		try{
+		    int d = 25/0;
+		    System.out.println(d);
+		}
+  		catch(ArithmeticException e) // Built in exception
+		{
+		    System.out.println(e);
+		}
+		catch(Exception e) // catch any exception
+		{
+		    System.out.println(e);
+		}
+		finally
+		{
+		    System.out.println("Finally Always executed");
+		}
+		System.out.println("Rest code");
+	}
+}
+
+
+// Custom Exception
+class AgeException extends RuntimeException {
+    AgeException(String msg) {
+        super(msg);
+    }
+}
+
+public class exceptionhandling {
+    public static void main(String[] args) {
+        int age = 21;
+
+        try {
+            if (age < 18) {
+                throw new AgeException("Can't vote"); // user-defined exception
+            } else {
+                System.out.println("Congrats, you can vote");
+            }
+        } catch (AgeException e) {
+            System.out.println("Age less than 18 not allowed: " + e);
+        }
+
+        System.out.println("Rest of the code");
+    }
+}
+
+
+
+// 3: Exception Using Method
+public class Main {
+    public static void validate(int age) {
+        if (age < 18) {
+            throw new ArithmeticException("Can't vote");
+        } else {
+            System.out.println("Congrats, you can vote");
+        }
+    }
+
+    public static void main(String[] args) {
+        int age = 14;
+
+        try {
+            validate(age);
+        } catch (ArithmeticException e) {
+            System.out.println("Age less than 18 not allowed: " + e.getMessage());
+        }
+
+        System.out.println("Rest of the code");
+    }
+}
+
---
+++ main.java
@@ -0,0 +1,14 @@
+class main {
+   static String UniName = "LU";
+   String name;
+   int id;
+
+   Main() {
+   }
+
+   void display(int n, String s) {
+      this.name = s;
+      this.id = n;
+      System.out.println(this.name + " " + this.id);
+   }
+}
