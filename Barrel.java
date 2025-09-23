dcd735725c50df436561068aa2fa8a3a3d477acb
Tarannummm <tarannumelina@gmail.com>
2025-09-24 00:13:58 +0600

    barrel roll

---
+++ Barrel.java
@@ -0,0 +1,15 @@
+public class Barrel {
+    public static void main(String[] args) {
+        while (true) {
+            System.out.println(
+              " (•_•)         \n" +
+              "<)   )╯   ↻   \n" +
+              " /   \\        \n" +
+              "\\ (•_•)       \n" +
+              " (   (>   ↻   \n" +
+              " /   \\        \n"
+              
+            );
+        }
+    }
+}
